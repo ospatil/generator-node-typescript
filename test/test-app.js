cf68c9616b94cf77f6fa1988028e9eb22094bf79
Patil, Omkar <Omkar.Patil@sungard.com>
2015-10-28 21:09:38 -0400

    Changes to avoid travis timeout

--- test/test-app.js
+++ test/test-app.js
@@ -6,8 +6,6 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('node-ts:app', function () {
-  //Needs only 5 seconds for running tsd and tsd install node --save but keeping it for 30 seconds for travis
-  this.timeout(5000);
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({ skipInstall: true })
@@ -36,7 +34,6 @@ describe('node-ts:app', function () {
     assert.file([
       '.vscode/tasks.json',
       'typings/app.d.ts',
-      'typings/node',
       'gulpfile.js',
       'tsconfig.json',
       'tslint.json',
