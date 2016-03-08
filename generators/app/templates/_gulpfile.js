b69b03daa648e35fc42d52db69d145383b691bcf
Omkar Patil <ospatil@gmail.com>
2016-03-08 10:50:12 -0500

    Add watch task

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -45,17 +45,30 @@ gulp.task('gen-def', 'Generate a single .d.ts bundle containing external module 
 });
 
 gulp.task('_build', 'INTERNAL TASK - Compiles all TypeScript source files', function (cb) {
+  exec('tsc --version', function (err, stdout, stderr) {
+    console.log('TypeScript ', stdout);
+    if (stderr) {
+      console.log(stderr);
+    }
+  });
+
   return exec('tsc', function (err, stdout, stderr) {
     console.log(stdout);
-    console.log(stderr);
+    if (stderr) {
+      console.log(stderr);
+    }
     cb(err);
   });
 });
 
-//run tslint task, then run _tsconfig_files and _gen_tsrefs in parallel, then run _build
+//run tslint task, then run update-tsconfig and gen-def in parallel, then run _build
 gulp.task('build', 'Compiles all TypeScript source files and updates module references', gulpSequence('tslint', ['update-tsconfig', 'gen-def'], '_build'));
 
 gulp.task('test', 'Runs the Jasmine test specs', ['build'], function () {
   return gulp.src('test/*.js')
     .pipe(jasmine());
+});
+
+gulp.task('watch', 'Watches ts source files and runs build on change', function () {
+  gulp.watch('src/**/*.ts', ['build']);
 });
