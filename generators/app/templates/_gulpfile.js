52d3c5875491ee584ddb1b7b194326035ecdb2ab
Omkar Patil <ospatil@gmail.com>
2016-08-06 11:22:37 -0400

    Upgrade to typescript 2.0 beta and tslint@next

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -3,25 +3,14 @@ var tslint = require('gulp-tslint');
 var exec = require('child_process').exec;
 var jasmine = require('gulp-jasmine');
 var gulp = require('gulp-help')(gulp);
-var tsconfig = require('gulp-tsconfig-files');
 var path = require('path');
-var inject = require('gulp-inject');
-var gulpSequence = require('gulp-sequence');
 var del = require('del');
-var dtsGenerator = require('dts-generator');
+var tslintCustom = require('tslint'); // for tslint-next https://github.com/panuhorsmalahti/gulp-tslint#specifying-the-tslint-module
 require('dotbin');
 
 var tsFilesGlob = (function (c) {
   return c.filesGlob || c.files || '**/*.ts';
 })(require('./tsconfig.json'));
-
-var appName = (function (p) {
-  return p.name;
-})(require('./package.json'));
-
-gulp.task('update-tsconfig', 'Update files section in tsconfig.json', function () {
-  gulp.src(tsFilesGlob).pipe(tsconfig());
-});
 
 gulp.task('clean', 'Cleans the generated js files from lib directory', function () {
   return del([
@@ -31,20 +20,14 @@ gulp.task('clean', 'Cleans the generated js files from lib directory', function 
 
 gulp.task('tslint', 'Lints all TypeScript source files', function () {
   return gulp.src(tsFilesGlob)
-    .pipe(tslint())
-    .pipe(tslint.report('verbose'));
+    .pipe(tslint({
+      tslint: tslintCustom,
+      formatter: "verbose"
+    }))
+    .pipe(tslint.report());
 });
 
-gulp.task('gen-def', 'Generate a single .d.ts bundle containing external module declarations exported from TypeScript module files', function (cb) {
-  return dtsGenerator.default({
-    name: appName,
-    project: '.',
-    out: './lib/' + appName + '.d.ts',
-    exclude: ['node_modules/**/*.d.ts', 'typings/**/*.d.ts']
-  });
-});
-
-gulp.task('_build', 'INTERNAL TASK - Compiles all TypeScript source files', function (cb) {
+gulp.task('build', 'INTERNAL TASK - Compiles all TypeScript source files', ['tslint'], function (cb) {
   exec('tsc --version', function (err, stdout, stderr) {
     console.log('TypeScript ', stdout);
     if (stderr) {
@@ -59,11 +42,6 @@ gulp.task('_build', 'INTERNAL TASK - Compiles all TypeScript source files', func
     }
     cb(err);
   });
-});
-
-//run tslint task, then run update-tsconfig and gen-def in parallel, then run _build
-gulp.task('build', 'Compiles all TypeScript source files and updates module references', function(callback) {
-  gulpSequence('tslint', ['update-tsconfig', 'gen-def'], '_build')(callback);
 });
 
 gulp.task('test', 'Runs the Jasmine test specs', ['build'], function () {
